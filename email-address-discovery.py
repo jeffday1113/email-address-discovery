5c7da69c2e55054b1dfe3bc3bd301e0135e17e9c
Jeffrey Day <jeffday1113@gmail.com>
2016-08-27 14:57:42 -0400

    Add runtime option to only look at one page's links for emails

--- email-address-discovery.py
+++ email-address-discovery.py
@@ -1,10 +1,19 @@
 from emaildiscoverer import EmailDiscoverer
+import argparse
 from bs4 import BeautifulSoup
 import requests
 from urlparse import urlparse
 
 if __name__ == '__main__':
-    disc = EmailDiscoverer('mit.edu')
+    parser = argparse.ArgumentParser(description='Program to print all emails found on discoverable pages in a given '
+                                                 'domain')
+    parser.add_argument('domain_name', metavar='domain_name', help='Domain name for which to search web pages'
+                                                                              ' for email addresses')
+    parser.add_argument('--limit_discovery_distance', help='Limits search for discoverable pages to only '
+                                                           'links found on initial input page', action='store_true')
+    args = parser.parse_args()
+    assert args.domain_name
+    disc = EmailDiscoverer(args.domain_name, args.limit_discovery_distance)
     #print disc.subdomainBoolean('www.technology.mit.edu')
     disc.discover_emails()
     # r = requests.get('http://jana.com')
--- emaildiscoverer.py
+++ emaildiscoverer.py
@@ -1,28 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-from requests import HTTPError
 from collections import deque
-from urlparse import urlparse
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
+from urlparse import urlparse
 import re
 
 
 class EmailDiscoverer(object):
 
-    def __init__(self, domain_name):
+    def __init__(self, domain_name, limit_discovery_distance):
         self.domain_name = domain_name
         self.visited_pages = set()
         self.pages_queue = deque()
         self.pages_queue.append(domain_name)
         self.email_set = set()
+        self.limit_discovery_distance = limit_discovery_distance
 
     def discover_emails(self):
         driver = webdriver.PhantomJS()
         try:
+            print "Found these email addresses:"
+            counter = 0
             while len(self.pages_queue) > 0:
-                #print len(self.pages_queue)
                 current_page = self.pages_queue.popleft()
+                #print self.limit_discovery_distance
                 #print current_page
                 if current_page not in self.visited_pages:
                     self.visited_pages.add(current_page)
@@ -30,39 +30,30 @@ class EmailDiscoverer(object):
                     driver.get('http://' + current_page)
                     if driver.current_url == current_url:
                         driver.get('http://www.' + current_page)
-                    # current_page = EmailDiscoverer.filterlink(current_page)
-                    #print current_page
-                    #print '\n'
-                    #print soup.find_all('a')
-                    #print current_page
-                    self.check_anchor_tags(driver.find_elements_by_tag_name("a"))
+                    self.check_anchor_tags(driver.find_elements_by_tag_name("a"), counter)
                     self.check_text(driver.find_elements_by_xpath("//*[contains(text(), '@')]"))
-                #print len(self.pages_queue)
+                    if counter == 0 and self.limit_discovery_distance:
+                        counter += 1
             driver.quit()
-            #print self.visited_pages
         except Exception as e:
             print e
             driver.quit()
 
-    def check_anchor_tags(self, elements):
+    def check_anchor_tags(self, elements, counter):
         for elem in elements:
             try:
                 linked_page = elem.get_attribute("href")
             except StaleElementReferenceException:
-                # print "jdfwejfoi"
                 continue
             if linked_page:
-                # print linked_page
                 if EmailDiscoverer.is_email(linked_page):
-                    # print linked_page
                     self.check_email_and_print(linked_page)
                     continue
-                # print linked_page
-                parsed_url = urlparse(linked_page)
-                if self.subdomain_boolean(parsed_url.netloc):
-                    if EmailDiscoverer.filter_link(linked_page) not in self.visited_pages:
-                        # print EmailDiscoverer.filterlink(linked_page)
-                        self.pages_queue.append(EmailDiscoverer.filter_link(linked_page))
+                if not self.limit_discovery_distance or counter == 0:
+                    parsed_url = urlparse(linked_page)
+                    if self.sub_domain_boolean(parsed_url.netloc):
+                        if EmailDiscoverer.filter_link(linked_page) not in self.visited_pages:
+                            self.pages_queue.append(EmailDiscoverer.filter_link(linked_page))
 
     def check_text(self, elements):
         for elem in elements:
@@ -95,21 +86,11 @@ class EmailDiscoverer(object):
             link = link.replace('//', '')
         return link
 
-        # if link.startswith('http://') or link.startswith('https://'):
-        #     return link
-        # else:
-        #     counter = 0
-        #     for letter in link:
-        #         if letter.isalnum():
-        #             break
-        #         counter += 1
-        #     return 'http://' + link[counter:]
-
     @staticmethod
     def add_http(domain):
         return 'http://' + domain
 
-    def subdomain_boolean(self, domain):
+    def sub_domain_boolean(self, domain):
         if self.domain_name.startswith('www'):
             return (self.domain_name == domain) or (self.domain_name[self.domain_name.index('.') + 1:] == domain)
         else:
